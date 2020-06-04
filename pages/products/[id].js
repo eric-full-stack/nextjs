dca8b0ddd4b2e515ad05d60358b04b1c5b9d5357
ericrprates <ericrprates@gmail.com>
2020-06-04 16:12:36 -0300

    fixed product[id] page

---
+++ pages/products/[id].js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../../components/layout";
+
+export default function Product() {
+  return (
+    <Layout>
+      <Head>
+        <title>{siteTitle} - Produto</title>
+      </Head>
+      <section>
+        <p>Produto teste</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
